577aa0c14d0edf9b13554ae35735597576735040
unknown <madodasmusic@gmail.com>
2020-08-16 23:51:27 -0700

    add folder manager functions

--- mm_uploads/google_drive/folder_manager.py
+++ mm_uploads/google_drive/folder_manager.py
@@ -1,23 +1,27 @@
 from __future__ import print_function
 import datetime
-# from googleapiclient.discovery import build
 import os
 from gdrive import Gdive
-from google_drive.gdrive import Gdive
-# from .auth import Auth
+from pathlib import Path
+from auth import Auth
+from accounts_manager import AccountsManager
 
 class FolderManager(Gdive):
     def __init__(self, gdrive_service):
-        self.service = gdrive_service
-        self.main_drive_folder_id = main_drive_folder_id
-        self.__content_folder_id = ""
-        self.copy_folders_ids = []
-        self.log_folder_id = ""
         super().__init__()
+        self.service = gdrive_service
+        if self.conf.get("main_folder_id", 0):
+            self.main_drive_folder_id = self.conf["main_folder_id"]
+        else:
+            self.main_drive_folder_id = "root"
+        
+        self._content_folder_id = 0
+        self._gdrive_path_folders_ids = []
+        self.__drive_log_folder_name = "logs"
+        self.__log_folder_id = 0
+        self.__base_log_folder_id = 0
+        
 
-    
-    def __init_conf(self):
-        self.__conf_file = Path("")
     
     def _ceackFolder(self, folder_name, parent_folder_id):
         page_token = None
@@ -54,26 +58,9 @@ class FolderManager(Gdive):
         return file.get('id')
 
 
-    
-    def _get_base_folder_id(self):
-        main_folder_name = str(datetime.datetime.today().year)
-        sub_folder_name = str(datetime.datetime.today().month)
-
-        main_folder_id = self._ceackFolder(main_folder_name, self.main_drive_folder_id)
-        if main_folder_id:
-            sub_folder_id = self._ceackFolder(sub_folder_name, main_folder_id)
-            if not sub_folder_id:
-                sub_folder_id = self.createNewFolder(sub_folder_name, main_folder_id)        
-        else:
-            main_folder_id = self.createNewFolder(main_folder_name, self.main_drive_folder_id)
-            sub_folder_id = self.createNewFolder(sub_folder_name, main_folder_id)
-        
-        return sub_folder_id
-
-    
-    
+            
     def create(self, folders):
-        base_folder_id = self._get_base_folder_id()
+        base_parent_folder_id, base_folder_id = self.create_gdrive_folder_from_date_Y_M(self.main_drive_folder_id)
         for index, folder in enumerate(folders):
             parent_id = folders[index -1]
             
@@ -82,14 +69,73 @@ class FolderManager(Gdive):
             content_folder_id = self._ceackFolder(folder, parent_id)
             if not content_folder_id:
                 content_folder_id = self.createNewFolder(folder, parent_id)
+            
+            self._gdrive_path_folders_ids.append(content_folder_id)
+            self._content_folder_id = content_folder_id
         
+        self._create_gdrive_log_folder_id()
+            
         return content_folder_id
-                
     
-    # def getLogFolderID(self):
+    
+    
+    def create_gdrive_folder_from_date_Y_M(self, main_parent_id=None):
+        year_folder_name = str(datetime.datetime.today().year)
+        month_folder_name = str(datetime.datetime.today().month)
+        if not main_parent_id: main_parent_id = self.main_drive_folder_id
+        year_folder_id = self._ceackFolder(year_folder_name, main_parent_id)
+        if year_folder_id:
+            month_folder_id = self._ceackFolder(month_folder_name, year_folder_id)
+            if not month_folder_id:
+                month_folder_id = self.createNewFolder(month_folder_name, year_folder_id)        
+        else:
+            year_folder_id = self.createNewFolder(year_folder_name, main_parent_id)
+            month_folder_id = self.createNewFolder(month_folder_name, year_folder_id)
+        
+        return year_folder_id, month_folder_id
+
+
+    
+    def _create_gdrive_log_folder_id(self):
+        self.__base_log_folder_id  = self._ceackFolder(self.__drive_log_folder_name, self.main_drive_folder_id)
+        if not self.__base_log_folder_id:
+            self.__base_log_folder_id = self.createNewFolder(self.__drive_log_folder_name, self.main_drive_folder_id)
+        
+        parent_folder_id, content_folder_id = self.create_gdrive_folder_from_date_Y_M(self.__base_log_folder_id)
+        self.__log_folder_id = content_folder_id
+        return content_folder_id
         
 
 
-fm = FolderManager("service")
 
-print(fm.conf["main_folder_id"])
+                
+    @property
+    def content_folder_id(self):
+        return self._content_folder_id
+    
+    @property
+    def gdrive_path_folders_ids(self):
+        return self._gdrive_path_folders_ids
+    
+    @property
+    def log_folder_id(self):
+        return self.__log_folder_id
+    
+    @property
+    def base_log_folder_id(self):
+        return self.__base_log_folder_id
+        
+
+if __name__ == "__main__":
+        am = AccountsManager()
+        account = am.getCred(__file__)
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+        auth = Auth(SCOPES, account)
+        creds = auth.getCreds()
+        service = build('drive', 'v3', credentials=creds)
+        fm = FolderManager(service)
+        fm.create("madoda", "manager")
+
+        print("base log folder id: ",fm.base_log_folder_id)
+        print("log folder id: ",fm.log_folder_id)
+        print("content folder id: ",fm.content_folder_id)
--- mm_uploads/google_drive/gdrive.py
+++ mm_uploads/google_drive/gdrive.py
@@ -1,23 +1,49 @@
 from pathlib import Path
 import json
 import os
-import sys
 class Gdive:
     def __init__(self):
         self.main_path = Path(__file__).parent.parent.parent.absolute()
+        self.__main_conf_path = Path(str(self.main_path)) / str("assets/main_conf.json")
+        self.__main_conf_key_name = "google_drive"
         self.__conf = {}
         self.__init_conf()
-        print("gdrive")
 
     
     def __init_conf(self):
-        conf_path = Path(str(self.main_path)) / str("assets/google_drive/conf.json")
-        if conf_path.exists():
-            self.__conf = json.loads( conf_path.read_text() )
+        if self.__main_conf_path.exists():
+            conf = json.loads( self.__main_conf_path.read_text() )
+            self.__conf = conf[self.__main_conf_key_name]
+
+
+
+    def __update_main_conf(self):
+        if self.__main_conf_path.exists():
+            conf = json.loads( self.__main_conf_path.read_text() )
+  
+            if conf.get("google_drive", 0):
+                conf[self.__main_conf_key_name] = self.__conf
+            else:
+                conf.setdefault(self.__main_conf_key_name, self.conf)
+          
+            self.__main_conf_path.write_text(json.dumps(conf))
+        
     
+    
+    def update_conf(self, key, value):
+        if self.__conf.get(key, 0):
+            self.__conf[key] = value
+        else:
+            self.__conf.setdefault(key, value)
+
+        self.__update_main_conf()
+
     @property
     def conf(self):
         return self.__conf
 
-print(sys.path)
-# Gdive()
+
+
+gd = Gdive()
+gd.update_conf("backup_id", "11111111")
+# print(gd.conf)
